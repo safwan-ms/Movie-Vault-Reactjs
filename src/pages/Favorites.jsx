367c83c017798a4167887747e068141ab1329ea0
safwan-ms <safwan.anasms@gmail.com>
2025-05-15 16:12:26 +0530

    style: Utilize responsive card layout for every device

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -4,7 +4,7 @@ import MovieCard from "../components/MovieCard";
 function Favorites() {
   const { favorites } = useMovieContext();
 
-  if (favorites) {
+  if (favorites.length > 0) {
     return (
       <div className="flex justify-center flex-col">
         <h2 className="text-3xl font-bold text-center text-white md:text-4xl mb-6 relative inline-block before:absolute before:-bottom-1 before:left-1/2 before:-translate-x-1/2 before:w-16 before:h-1 before:bg-indigo-500 my-10">
@@ -17,14 +17,18 @@ function Favorites() {
         </div>
       </div>
     );
+  } else {
+    return (
+      <div className="justify-center flex flex-col">
+        <h2 className="text-3xl font-bold text-center text-white md:text-4xl mb-6 relative inline-block before:absolute before:-bottom-1 before:left-1/2 before:-translate-x-1/2 before:w-16 before:h-1 before:bg-indigo-500 my-10">
+          No Favorite Movies Yet
+        </h2>
+        <p className="text-center">
+          Start adding movies to your favorites and they will appear here!
+        </p>
+      </div>
+    );
   }
-
-  return (
-    <div className="favorites-empty">
-      <h2>No Favorite Movies Yet</h2>
-      <p>Start adding movies to your favorites and they will appear here!</p>
-    </div>
-  );
 }
 
 export default Favorites;
